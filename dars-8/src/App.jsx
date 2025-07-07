26b5cb3e8ad635ef3b860cfb4290556c73777826
Diyor-Khasanov <baxacap@gmail.com>
2025-07-07 09:18:36 +0300

    dars-8. custom hook, memo, useMemo, useCallback, controlled and uncontrolled components

---
+++ dars-8/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+// | Tushuncha              | Sodda Tushuntirish        | Kundalik Hayot Misoli        |
+// | ---------------------- | ------------------------- | ---------------------------- |
+// | Custom Hook            | O‘zing tayyorlagan hook   | Tayyor retsept               |
+// | memo                   | Komponentni yodda saqlash | Chexol – tayyor sumka        |
+// | useMemo                | Qiymatni yodda saqlash    | Eski hisobni qayta ishlatish |
+// | useCallback            | Funksiyani yodda saqlash  | Tayyor do‘stni chaqirish     |
+// | Controlled Component   | React boshqaradi          | Robot sizga buysunadi        |
+// | Uncontrolled Component | DOM o‘zi boshqaradi       | Mashina o‘zi yuradi          |
+
+// ✅ Custom hook → kodni qayta ishlatish uchun.
+// ✅ memo → keraksiz chizishdan saqlaydi.
+// ✅ useMemo → og‘ir hisobni qayta qilmaslik uchun.
+// ✅ useCallback → funksiyani bekorga qayta yaratmaslik uchun.
+// ✅ Controlled → to‘liq siz boshqarasiz.
+// ✅ Uncontrolled → o‘zi boshqaradi.
+
+function App() {
+
+
+  return <div>App</div>;
+}
+export default App;
